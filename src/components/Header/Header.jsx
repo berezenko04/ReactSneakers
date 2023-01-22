ba00124f3808e45341444e8b067562070aa9a29f
Roman <53516467+berezenko04@users.noreply.github.com>
2023-01-22 18:15:15 +0200

    refactored style hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,77 +22,71 @@ const Header = ({ onClickCart }) => {
 
     const totalPrice = cartItems.reduce((acc, obj) => acc + obj.price, 0);
 
+    const handleClickHamburgerMenu = () => {
+        setIsMenuOpened(!isMenuOpened);
+    }
+
     return (
         <header className={styles.header}>
             <div className="container">
                 <div className={styles.header__wrapper}>
-                    <Link to='/ReactSneakers/'>
-                        <div className={styles.header__left}>
-                            <img src={Logo} alt="logo" />
-                            <div className={styles.header__left__text}>
-                                <h3>React Sneakers</h3>
-                                <p>Магазин лучших кроссовок</p>
+                    <div className={styles.header__main}>
+                        <Link to='/ReactSneakers/'>
+                            <div className={styles.header__left}>
+                                <img src={Logo} alt="logo" />
+                                <div className={styles.header__left__text}>
+                                    <h3>React Sneakers</h3>
+                                    <p>Магазин лучших кроссовок</p>
+                                </div>
                             </div>
+                        </Link>
+                        <div
+                            className={styles.header__burger}
+                            onClick={handleClickHamburgerMenu}
+                        >
+                            <MenuIcon className={styles.menu} />
                         </div>
-                    </Link>
-                    <div
-                        className={styles.header__burger}
-                        onClick={() => setIsMenuOpened(true)}
-                    >
-                        <MenuIcon className={styles.menu} />
+                        <ul className={styles.header__right}>
+                            <li onClick={onClickCart}>
+                                <button className={styles.header__right__cart}>
+                                    <CartIcon />
+                                    <span>{totalPrice} ₴</span>
+                                </button>
+                            </li>
+                            <li><Link to='/ReactSneakers/favorites' className={styles.header__right__favorite}><FavoriteIcon /></Link></li>
+                            <li><Link to='/ReactSneakers/orders' className={styles.header__right__user}><UserIcon /></Link></li>
+                        </ul>
                     </div>
                     {isMenuOpened &&
-                        <div className={styles.header__burger__opened}>
-                            <div className={styles.header__burger__body}>
-                                <div className={styles.header__burger__body__head}>
-                                    <button
-                                        onClick={() => setIsMenuOpened(false)}
-                                    >
-                                        <CancelIcon className={styles.cancel} />
-                                    </button>
-                                </div>
-
-                                <ul className={styles.header__burger__body__menu}>
-                                    <li onClick={onClickCart}>
-                                        <button className={styles.header__burger__body__menu__cart} onClick={() => setIsMenuOpened(false)}>
-                                            <CartIcon />
-                                            <span>Корзина</span>
-                                        </button>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            to='/ReactSneakers/favorites'
-                                            className={styles.header__burger__body__menu__favorite}
-                                            onClick={() => setIsMenuOpened(false)}
-                                        >
-                                            <FavoriteIcon />
-                                            Мои закладки
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            to='/ReactSneakers/orders'
-                                            className={styles.header__burger__body__menu__orders}
-                                            onClick={() => setIsMenuOpened(false)}
-                                        >
-                                            <UserIcon />
-                                            Мои заказы
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                        <ul className={styles.header__burger__opened}>
+                            <li onClick={onClickCart}>
+                                <button onClick={() => setIsMenuOpened(false)}>
+                                    <CartIcon className={styles.icon} />
+                                    <span>Корзина</span>
+                                </button>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/ReactSneakers/favorites'
+                                    className={styles.header__burger__body__menu__favorite}
+                                    onClick={() => setIsMenuOpened(false)}
+                                >
+                                    <FavoriteIcon className={styles.icon} />
+                                    Мои закладки
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/ReactSneakers/orders'
+                                    className={styles.header__burger__body__menu__orders}
+                                    onClick={() => setIsMenuOpened(false)}
+                                >
+                                    <UserIcon className={styles.icon} />
+                                    Мои заказы
+                                </Link>
+                            </li>
+                        </ul>
                     }
-                    <ul className={styles.header__right}>
-                        <li onClick={onClickCart}>
-                            <button className={styles.header__right__cart}>
-                                <CartIcon />
-                                <span>{totalPrice} ₴</span>
-                            </button>
-                        </li>
-                        <li><Link to='/ReactSneakers/favorites' className={styles.header__right__favorite}><FavoriteIcon /></Link></li>
-                        <li><Link to='/ReactSneakers/orders' className={styles.header__right__user}><UserIcon /></Link></li>
-                    </ul>
                 </div>
             </div>
         </header >
